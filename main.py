cc4ccb163ac9942a1057ee230bb8a978c61334be
phuongDoMega <pdmega268@gmail.com>
2024-09-19 16:13:23 +0700

    .

--- main.py
+++ main.py
@@ -18,10 +18,11 @@ client = discord.Client(intents=intents)
 
 GUILD_ID=1122707918177960047
 RESULT=None
+THREADS=[]
 
 @client.event
 async def on_ready():
-    global RESULT
+    global RESULT,THREADS
     try:
         req=requests.get('http://localhost:8888')
         print(req.status_code)
@@ -32,41 +33,62 @@ async def on_ready():
         server.b()  
         guild = client.get_guild(GUILD_ID)
         RESULT=await getBasic(guild)
+        for thread in RESULT['threadsCh'].threads:
+            msgs=[msg async for msg in thread.history(oldest_first=True)]
+            THREADS.append({
+                'username':msgs[0].content.split('@')[1],
+                'lastMsg':msgs[-1]
+            })
         result = await getBasic(guild)
-        if not updateData.is_running():
-            updateData.start(guild)
+        
         if not playing.is_running():
             playing.start(guild)
-@tasks.loop(seconds=30)
+        await asyncio.sleep(60)
+        if not updateData.is_running():
+            updateData.start(guild)
+@tasks.loop(seconds=60)
 async def updateData(guild):
-    global RESULT
-    RESULT=await getBasic(guild)
-    usernames=[]
-    for msg in RESULT['usernames']:
-        usernames.append(msg.content)
-    for user in usernames:
-        email=user.split('||')[1].replace('``','')
-        isset=False
+    global RESULT,THREADS
+    THREADS=[]
+    print('updateData is running')
+    try:
+        RESULT=await getBasic(guild)
+        usernames=[]
+        for msg in RESULT['usernames']:
+            usernames.append(msg.content)
+        for user in usernames:
+            email=user.split('||')[1].replace('``','')
+            isset=False
+            for thread in RESULT['threadsCh'].threads:
+                if email==thread.name:
+                    isset=True
+            if not isset:
+                rs=await RESULT['threadsCh'].create_thread(name=user.split('||')[1].replace('``',''),content='https://suno.com/@'+user.split('||')[0].replace('``','').lower())
+                req=requests.get('https://suno.com/@'+user.split('||')[0].replace('``',''))
+                soup=BeautifulSoup(req.text,'html.parser')
+                els=soup.find('div',{'class':'flex flex-col lg:flex-row gap-8 px-4 pb-8 lg:p-8 pt-16 items-center -mt-32 md:mt-0 z-[20]'}).find('div',{'class':'flex flex-col lg:flex-row gap-8 w-full lg:w-auto'}).find('div',{'class':'flex flex-col'}).find('div',{'class':'flex flex-row gap-2'}).find_all('div')
+                await rs.thread.send(content=f"``LIKE``== **{els[0].getText().split(' ')[0]}** | ``FANS``== **{els[1].getText().split(' ')[0]}** | ``PLAYS``== **{els[2].getText().split(' ')[0]}**")
         for thread in RESULT['threadsCh'].threads:
-            if email==thread.name:
-                isset=True
-        if not isset:
-            rs=await RESULT['threadsCh'].create_thread(name=user.split('||')[1].replace('``',''),content='https://suno.com/@'+user.split('||')[0].replace('``','').lower())
-            req=requests.get('https://suno.com/@'+user.split('||')[0].replace('``',''))
-            soup=BeautifulSoup(req.text,'html.parser')
-            els=soup.find('div',{'class':'flex flex-col lg:flex-row gap-8 px-4 pb-8 lg:p-8 pt-16 items-center -mt-32 md:mt-0 z-[20]'}).find('div',{'class':'flex flex-col lg:flex-row gap-8 w-full lg:w-auto'}).find('div',{'class':'flex flex-col'}).find('div',{'class':'flex flex-row gap-2'}).find_all('div')
-            await rs.thread.send(content=f"``LIKE``== **{els[0].getText().split(' ')[0]}** | ``FANS``== **{els[1].getText().split(' ')[0]}** | ``PLAYS``== **{els[2].getText().split(' ')[0]}**")
+            try:
+                msgs=[msg async for msg in thread.history(oldest_first=True)]
+                THREADS.append({
+                    'username':msgs[0].content.split('@')[1],
+                    'lastMsg':msgs[-1]
+                })
+            except:
+                pass
+    except:
+        pass
 @tasks.loop(seconds=1)
 async def playing(guild):
     print('playing is running')
-    global RESULT
-    for thread in RESULT['threadsCh'].threads:
+    global RESULT,THREADS
+    for thread in THREADS:
         try:
-            msgs=[msg async for msg in thread.history(oldest_first=True)]
-            username=msgs[0].content.split('@')[1]
-            url='https://clerk.suno.com/v1/client/sessions/sess_2mDnGfr55VzPNAqZFq6Wa4kPzq8/touch?_clerk_js_version=5.21.2'
+            username=thread['username']
+            url='https://clerk.suno.com/v1/client/sessions/sess_2mHWu8j9M0ICNeeYSIyofy0XnLE/touch?_clerk_js_version=5.22.3'
             headers={
-                'cookie':'__cf_bm=Z_sB6bARq8DGnkZm7oYfQsxiUSvZAWbSwE5_yFEhaMI-1726629712-1.0.1.1-SJ_7i_m7fDZxC17bgWnRGfgaTGpP1IIn88QvZfwnxAOhiBBCSen5X5HGUdWiPTP1.EFGREtlZFd_F6Xc4k0Kwg; _cfuvid=H8yFhqqMQd7crZp6vEgjJgy6XzcNFe12N8mez8cQ58c-1726619489684-0.0.1.1-604800000; __client_uat=1726619919; __client=eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6ImNsaWVudF8ybURuOElFY25JNk9HNFROWGYzYmJqU1M0cVAiLCJyb3RhdGluZ190b2tlbiI6InU1MzUxdTlieHBjM2QzZTFmOWhyMG1ka3R4bXp6Y3Vva2M2amp0bGQifQ.CeCJIu6Vdy7KyBsgN7oCaLCbPIYfN7pOGJ5w7qY2qil7Jrnn427cd6FkrtUK2P0fQdrcv2-UH5QlwgqyrhHfAuUOeAJEqy-7Enxwc0GgkwkTTCYvLlPbBQ9kLjivaUKQqfcGV5GSwsIYE8shdQvBPAheq8zuBEeZTOkQ1v3xcZofuNoTgrJOMERvmw71xk9v2GyY5Gw0qXL0CndVCxOWTtcHjyfc3NAgqIXSJhISw5SxOFFd5ZAynEse6m3M44iCAxu7Sr9e1vGjN67huxXFb1nQI5s0L5IY38RYDaEHNsVwL9llgdnN6ftkBqbI5RlaseWwzSRP0glj9xWHHHT_dQ',
+                'cookie':'__cf_bm=0rbeCSFyX_1QKzLm064UwuYS71pzk.OXB28I6R06JMc-1726734155-1.0.1.1-ix0rwkEKHYBy3dDAYaWT2mLHHyd_tHW0Eu8zju6H66S.FTxVzcUYy0RvtLDCQUyclqIQNvqawukvoK0rzvSnvQ; _cfuvid=vEoQsXOqn7_RWffFtnozG8vzWnITezQxO_EAoIRIyyg-1726734155372-0.0.1.1-604800000; __client_uat=1726734201; __client_uat_U9tcbTPE=1726734201; __client=eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6ImNsaWVudF8ybUhXcERxaTFJZjBESE9ySHQzQTBNcTZwanQiLCJyb3RhdGluZ190b2tlbiI6Im0yaTUwbGxpazZlZ2djdmE3ZGx3MDgxczdvbXlldmdjYzZqYTExNjkifQ.CVW3p1cWsWYryKyLgY2ScazeJrxncZmVivCQjMfRfy2rnu59Z7F_cwpU_kRtxNQ5KwJ184MG1fqhvN-muvlQATt7WtQYBFMv6ya08dYgz_Yy7eXL0OXwtVuK0y0Z95jXNJOLTdwc4_qXGSUcu60cCoZirB306BcmmD1ty_7jd0jxsDd01YVyYPJan5hkyvyh9bl2hOjquDXZXVcfblX6vVtu8hfjdv5e5wO7ByDoi4ki-SHWDx6q3dNvhwl0u7xbWwjRuQpQf7z3Ed4ExvreoX11IzgwjlDIst4o598LE8ivz8gYGdP5eQpgkYXEE49UqNLd3ANrfdfUIboy3IW21A; ajs_anonymous_id=ae0a5283-be14-470f-8d3b-e3969fdb2c42',
                 'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36 Edg/113.0.1774.9',
             }
             data={
@@ -92,7 +114,7 @@ async def playing(guild):
                     req=requests.get('https://suno.com/@'+username)
                     soup=BeautifulSoup(req.text,'html.parser')
                     els=soup.find('div',{'class':'flex flex-col lg:flex-row gap-8 px-4 pb-8 lg:p-8 pt-16 items-center -mt-32 md:mt-0 z-[20]'}).find('div',{'class':'flex flex-col lg:flex-row gap-8 w-full lg:w-auto'}).find('div',{'class':'flex flex-col'}).find('div',{'class':'flex flex-row gap-2'}).find_all('div')
-                    await msgs[-1].edit(content=f"``LIKE``== **{els[0].getText().split(' ')[0]}** | ``FANS``== **{els[1].getText().split(' ')[0]}** | ``PLAYS``== **{els[2].getText().split(' ')[0]}**")
+                    await thread['lastMsg'].edit(content=f"``LIKE``== **{els[0].getText().split(' ')[0]}** | ``FANS``== **{els[1].getText().split(' ')[0]}** | ``PLAYS``== **{els[2].getText().split(' ')[0]}**")
         except:
             pass
 client.run(os.environ.get('botToken'))
